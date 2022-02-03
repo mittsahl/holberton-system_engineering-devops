db6a7bb1cba213037cc05f5716ba4aef00b69d41
mittsahl <msalem311@aol.com>
2022-02-03 13:42:22 +0000

    Added comma

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Runs bash script
 exec { 'example':
-  command => "sed -i 's/phpp.*/php/' /var/www/html/wp-settings.php"
+  command => "sed -i 's/phpp.*/php/' /var/www/html/wp-settings.php",
   path    => [ '/bin', '/sbin']
 }
